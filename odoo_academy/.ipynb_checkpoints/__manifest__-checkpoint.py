adf8e09a345ba2e627fc82115fd9cd8300b7bf83
ger1564 <gexo1564@gmail.com>
2021-04-12 17:29:27 +0000

    [Update]  2

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,25 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-
-    'name':'Odoo Academy',
-    'summary': """Academy app to manage Trainning"""',
+    'name': 'Odoo Academy',
+	'summary': """Academy app to manage Trainning""",
     'description': """
-                    Academy Module to manage Trainning:
+					Academy Module to manage Trainning:
                     -Courses
                     -Sessions
                     -Attendees
-                    """",
-    'author': 'Odoo',
-    'website': 'https://www.odoo.com',
+
+    """,
     'category': 'Trainning',
-    'version':'0.1',
-    'depends':'[base]',
+	'author':'Odoo',
+	'website': 'https://www.odoo.com',
+	'version':'0.1',
+    'depends': ['base'],
     'data': [
 
-
     ],
-    'demo':[
-
-
+    'demo': [
     ],
-        }
+
+}
